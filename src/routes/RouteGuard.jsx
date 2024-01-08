47c36c943f8b08bab734d67962480825ff13cb21
GerVaf <thantzinmin667@gmail.com>
2024-01-08 14:42:35 +0630

    v0.1

--- src/routes/RouteGuard.jsx
+++ src/routes/RouteGuard.jsx
@@ -2,11 +2,26 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import React from "react";
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode"; // You'll need to install jwt-decode
 
 export const RouteGuard = ({ children }) => {
   const token = Cookies.get("token");
+
   if (token) {
-    return children;
+    try {
+      const decodedToken = jwtDecode(token);
+      // console.log(decodedToken);
+      const currentTime = Date.now() / 1000;
+      // console.log(currentTime);
+      if (decodedToken.exp < currentTime) {
+        // Token is expired
+        return <Navigate to="/welcome" />;
+      }
+
+      return children;
+    } catch (error) {
+      return <Navigate to="/welcome" />;
+    }
   } else {
     return <Navigate to="/welcome" />;
   }
